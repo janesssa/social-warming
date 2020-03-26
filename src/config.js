f7619573b001a4125121a63e8056523586394ce9
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-03-26 17:36:34 +0100

    Config : added img object

--- src/config.js
+++ src/config.js
@@ -70,15 +70,31 @@ export const progress = {
           title: "Mindmap",
           subtitle: "Optioneel?",
           text: "Mindmapping is een creatieve techniek die helpt bij het leggen van connecties bij een onderwerp. In de mindmap staat een onderwerp centraal. Dit onderwerp breed je uit met verschillende takken, deze takken zijn woorden die je associeert bij het onderwerp. Deze takken worden nu het onderwerp van de volgende vertakkingen. Zo heb je uiteindelijk een heel web met begrippen sommige hiervan hebben met het hoofdonderwerp te maken en sommige ook niet. Wij hebben deze methode gebruikt omdat het een overzicht van het onderwerp geeft en alle onderdelen hier erbij hoorden. En tijdens het maken van de mindmapping kom je er achter als groep waar je nog dieper op in moet gaan.",
-          img: "./img/mindmapping",
+          img: {
+            src: "./img/mindmapping",
+            alt: "foto van mindmap"
+          }
         }
       },
       {
         content: {
-          title: "Foreced perspective",
+          title: "Forced perspective",
           subtitle: "Optioneel?",
           text: "Bij deze techniek ga je op zoek naar de onderliggende onderwerpen van een thema. Per thema schrijf je woorden op die bij je opkomen als je aan het thema denkt, zoveel mogelijk en zo snel mogelijk. Als dit voor alle thema’s is gedaan stem je per thema op de woorden die jij het best vindt. Zo eindige je per thema met een woord. Met deze woorden ga je concepten bedenken. Wij hebben als team deze techniek gebruikt omdat het ons werd aangeraden. En het forceert je om anders naar het onderwerp te kijken en je moet creatieve denken om deze woorden samen te brengen in een concept.",
-          img: ["./img/forced1","./img/forced2","./img/forced3"],
+          imgs: [
+            {
+              src: "./img/forced1",
+              alt: "eerste foto van forced perspective"
+            },
+            {
+              src: "./img/forced2",
+              alt: "tweede foto van forced perspective"
+            }, 
+            {
+              src: "./img/forced3",
+              alt: "derde foto van forced perspective"
+            }
+          ]
         }
       },
       {
@@ -93,7 +109,10 @@ export const progress = {
           title: "COCD box",
           subtitle: "Optioneel?",
           text: "De COCD box wordt gebruikt voor het beoordelen van concepten. Je maakt een kruis en zet 4 waarden aan elke kant van de lijnen. Als de waarden op dezelfde lijn liggen zijn het tegenovergestelde van elkaar. Per concept beoordeeld je waar deze hoort binnen de box. En aan de hand hiervan kan je de waarden van een concept achterhalen. Hiermee kan je makkelijker de beslissing maken met welke concepten je verder wilt gaan. WIj hebben deze techniek gebruikt om de concepten van de 3 x 3 x 3 sessie te beoordelen.",
-          img: ["./img/COCD-box"],
+          img: {
+            src: "./img/COCD-box",
+            alt: "foto van de COCD box"
+          }
         }
       },
       {
@@ -101,7 +120,10 @@ export const progress = {
           title: "Sketchboarding",
           subtitle: "Optioneel?",
           text: "Sketchboarding is een techniek die helpt bij het visualiseren van concepten. In plaats van tekst ga je je concept visueel maken. Met tekening laat je de flow zien van het concept. Dit zorgt ervoor dat het concept duidelijker. Wij hebben deze techniek gebruikt om onze concepten beter aan elkaar te kunnen presenteren. En op elkaars concepten te kunnen bouwen zodat we een definitieve sketch per concept konden maken.",
-          img: ["./img/sketchboarding"],
+          img: {
+            src: "./img/sketchboarding",
+            alt: "foto van de COCD box"
+          }
         }
       },
     ]
