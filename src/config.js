493ce915ccbfd415771c06e2bf1a1106ba0d1410
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-03-26 16:56:25 +0100

    Config : added designTactics text

--- src/config.js
+++ src/config.js
@@ -53,7 +53,8 @@ export const profiles = {
 // }
 
 export const intro = {
-  title: "Social warming",
+  firstTitle: "Social",
+  secondTitle: "Warming",
   text: "Op deze pagina staat vastgelegd hoe de groep social warming te werk gaat met de design challenge : ‘Maak een digitale toepassing over het thema social cooling voor Bevrijdingsdag 5 mei 2020.’. Op deze onepager staat ons creatief proces vastgelegd."
 }
 
@@ -61,21 +62,42 @@ export const progress = {
   designTactics: {
     firstTitle: "Ontwerp",
     secondTitle: "technieken",
-    intro: "Onderstaande technieken hebben we gebruikt om te ontwerpen.",
+    intro: "Dit is een overzicht van alle creatieve technieken die we hebben gebruikt om onze concepten te ontwikkelen.",
     bgColor: "black",
     contentBlocks: [
       {
         content: {
-          title: "Eerste taak",
+          title: "Mindmap",
           subtitle: "Optioneel?",
-          text: "Dit is de eerste taak die uitgevoerd is. Top!"
+          text: "Mindmapping is een creatieve techniek die helpt bij het leggen van connecties bij een onderwerp. In de mindmap staat een onderwerp centraal. Dit onderwerp breed je uit met verschillende takken, deze takken zijn woorden die je associeert bij het onderwerp. Deze takken worden nu het onderwerp van de volgende vertakkingen. Zo heb je uiteindelijk een heel web met begrippen sommige hiervan hebben met het hoofdonderwerp te maken en sommige ook niet. Wij hebben deze methode gebruikt omdat het een overzicht van het onderwerp geeft en alle onderdelen hier erbij hoorden. En tijdens het maken van de mindmapping kom je er achter als groep waar je nog dieper op in moet gaan."
         }
       },
       {
         content: {
-          title: "Tweede taak",
+          title: "Foreced perspective",
           subtitle: "Optioneel?",
-          text: "Dit is de tweede taak die uitgevoerd is. Top!"
+          text: "Bij deze techniek ga je op zoek naar de onderliggende onderwerpen van een thema. Per thema schrijf je woorden op die bij je opkomen als je aan het thema denkt, zoveel mogelijk en zo snel mogelijk. Als dit voor alle thema’s is gedaan stem je per thema op de woorden die jij het best vindt. Zo eindige je per thema met een woord. Met deze woorden ga je concepten bedenken. Wij hebben als team deze techniek gebruikt omdat het ons werd aangeraden. En het forceert je om anders naar het onderwerp te kijken en je moet creatieve denken om deze woorden samen te brengen in een concept."
+        }
+      },
+      {
+        content: {
+          title: "3 x 3 x 3",
+          subtitle: "Optioneel?",
+          text: "Een 3 x 3 x 3 is bedoeld om concepten te genereren, aan het einde van de sessie heb je per persoon 3 concepten. Per concept krijg je 3 minuten om deze te bedenken. Na 3 keer 3 minuten presenteer je de concepten en beoordeeld je deze.. Wij hebben deze techniek gebruikt omdat het je geforceerd om na te denken en concepent te maken. "
+        }
+      },
+      {
+        content: {
+          title: "COCD box",
+          subtitle: "Optioneel?",
+          text: "De COCD box wordt gebruikt voor het beoordelen van concepten. Je maakt een kruis en zet 4 waarden aan elke kant van de lijnen. Als de waarden op dezelfde lijn liggen zijn het tegenovergestelde van elkaar. Per concept beoordeeld je waar deze hoort binnen de box. En aan de hand hiervan kan je de waarden van een concept achterhalen. Hiermee kan je makkelijker de beslissing maken met welke concepten je verder wilt gaan. WIj hebben deze techniek gebruikt om de concepten van de 3 x 3 x 3 sessie te beoordelen."
+        }
+      },
+      {
+        content: {
+          title: "Sketchboarding",
+          subtitle: "Optioneel?",
+          text: "Sketchboarding is een techniek die helpt bij het visualiseren van concepten. In plaats van tekst ga je je concept visueel maken. Met tekening laat je de flow zien van het concept. Dit zorgt ervoor dat het concept duidelijker. Wij hebben deze techniek gebruikt om onze concepten beter aan elkaar te kunnen presenteren. En op elkaars concepten te kunnen bouwen zodat we een definitieve sketch per concept konden maken."
         }
       }
     ]
