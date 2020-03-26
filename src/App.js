36ab4ca4e6fd3ec9f41e43d5ff37506833730a2c
Rachelle Bosman <32331937+rachellebosman@users.noreply.github.com>
2020-03-26 10:49:20 +0100

    [TASK] try to merge

--- src/App.js
+++ src/App.js
@@ -1,49 +1,10 @@
 import React from "react";
 
-import ContentBlock from "./components/ContentBlock";
-import Image from "./components/Image";
-import Profile from "./components/Profile";
+import Team from "./components/Team";
+import Section from "./components/Section"
 import Header from "./components/Header";
 
 import { progress, profiles } from "./config";
-
-const ProgressList = ({ content }) => {
-  let imageLeft = false;
-  return (
-    <ul>
-      {content.map(item => {
-        if (imageLeft) {
-          imageLeft = !imageLeft;
-          return (
-            <li className='container'>
-              <Image data={item.img} />
-              <ContentBlock content={item.content} />
-            </li>
-          );
-        } else {
-          imageLeft = !imageLeft;
-          return (
-            <li className='container's>
-              <ContentBlock content={item.content} />
-              <Image data={item.img} />
-            </li>
-          );
-        }
-      })}
-    </ul>
-  );
-};
-
-const ProfileList = ({ team }) => {
-  console.log(team);
-  return (
-    <ul className='container'>
-      {team.forEach(profile => {
-        return <Profile profile={profile} />;
-      })}
-    </ul>
-  );
-};
 
 function App() {
   console.log(progress);
